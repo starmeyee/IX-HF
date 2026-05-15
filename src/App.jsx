59d5b6b9d0db2c454e3aee6e7a80d61e78811669
lohveraofficial <lohveraofficial@gmail.com>
2026-05-15 09:17:56 +0530

    Fix scroll position persisting across route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Homework from './pages/Homework';
 import HolidayHomework from './pages/HolidayHomework';
 import { Heart } from 'lucide-react';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="app-container">
         <Navbar />
         <main className="main-content">
